8fc247309b70abfd166802bd353541ce564c5a1c
apm <apm@mail.com>
2025-11-09 21:31:34 +0100

    T2L1E1 done

--- src/level1ex1/Main.java
+++ src/level1ex1/Main.java
@@ -1,13 +1,7 @@
 package level1ex1;
 
 public class Main {
-    private static void calculateTotalSale(Sale sale) {
-        try {
-            sale.calculateTotal();
-        } catch (SaleEmptyException e) {
-            System.out.println("ERROR : " + e.getMessage());
-        }
-    }
+
     public static void main(String[] args){
 
         Sale saleEmpty;
@@ -15,14 +9,14 @@ public class Main {
 
         saleEmpty = new Sale();
         System.out.println("Execute saleEmpty.calculateTotal()");
-        calculateTotalSale(saleEmpty);
+        saleEmpty.calculateTotalSale();
 
         System.out.println("\nCreate saleWithProducts' variable and add products:");
         saleWithProducts = new Sale();
         saleWithProducts.addProduct(new Product("product1", 1.0));
         saleWithProducts.addProduct(new Product("product2", 1.0));
         saleWithProducts.addProduct(new Product("product3", 1.0));
-        calculateTotalSale(saleWithProducts);
+        saleEmpty.calculateTotalSale();
         System.out.println(" The 'totalSalePrice' in 'saleWithProducts' -> " + saleWithProducts.getTotalSalePrice());
     }
 }
--- src/level1ex1/Sale.java
+++ src/level1ex1/Sale.java
@@ -6,6 +6,12 @@ public class Sale {
     private ArrayList<Product> productCollection;
     private Double totalSalePrice;
 
+    private  void innerCalculateTotal() throws SaleEmptyException{
+        if (productCollection.isEmpty())
+            throw new SaleEmptyException("To make a sale you first have to add products");
+        for (Product product : productCollection) totalSalePrice += product.getPrice();
+    }
+
     public Sale(){
         this.productCollection = new ArrayList<>();
         this.totalSalePrice = 0.0;
@@ -19,9 +25,11 @@ public class Sale {
         return totalSalePrice;
     }
 
-    public void calculateTotal() throws SaleEmptyException{
-       if (productCollection.isEmpty())
-                throw new SaleEmptyException("To make a sale you first have to add products");
-            for (Product product : productCollection) totalSalePrice += product.getPrice();
+    public void calculateTotalSale() {
+        try {
+            innerCalculateTotal();
+        } catch (SaleEmptyException e) {
+            System.out.println("ERROR : " + e.getMessage());
+        }
     }
 }
